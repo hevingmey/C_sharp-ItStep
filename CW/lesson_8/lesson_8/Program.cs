d8446c877b79b5935c87bf64dc3ee539bf91a416
hevingmey <grisenkov265@gmail.com>
2025-12-21 21:34:24 +0000

    add task 17_12_25

---
+++ CW/lesson_8/lesson_8/Program.cs
@@ -0,0 +1,192 @@
+﻿namespace lesson_8;
+
+abstract class Card
+{
+    public decimal Balance { get; set; }
+    public decimal Commission;
+    public Card (decimal balance, decimal commission)
+    {
+        Balance = balance;
+        Commission = commission;
+    }
+    public abstract void BalanceWriteOff(decimal amount);
+}
+
+class Monobank : Card
+{
+    public Monobank(decimal balance)
+        : base(balance, 2.0m){}
+
+    public override void BalanceWriteOff(decimal amount)
+    {
+        decimal money = amount+Commission;
+        Console.WriteLine($"Complete Monobank with {money} $");
+    }
+}
+
+class Privatbank : Card
+{
+    public Privatbank(decimal balance)
+        : base(balance, 20.0m){}
+
+    public override void BalanceWriteOff(decimal amount)
+    {
+        decimal money = amount+Commission;
+        Console.WriteLine($"Complete Privatebank with {money} $");
+    }
+}
+
+class Abank : Card
+{
+    public Abank(decimal balance)
+        : base(balance, 10.0m){}
+
+    public override void BalanceWriteOff(decimal amount)
+    {
+        decimal money = amount+Commission;
+        Console.WriteLine($"Complete Abank with {money} $");
+    }
+}
+
+class ATM
+{
+    public void BalanceWriteOff(Card card, decimal amount)
+    {
+        Console.WriteLine("Started ATM");
+        card.BalanceWriteOff(amount);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ATM atm = new ATM();
+        Card mono=new Monobank(1000m);
+        Card abank=new Abank(500m);
+        Card privat=new Privatbank(800m);
+            
+        atm.BalanceWriteOff(mono,250m);
+        atm.BalanceWriteOff(abank,450m);
+        atm.BalanceWriteOff(privat,50m);
+    }
+}
+
+
+
+
+
+
+
+// public interface IOn
+// {
+//     void on();
+// }
+//
+// public interface IOff
+// {
+//     void off();
+// }
+//
+// public interface ICopy
+// {
+//     void copy();
+// }
+//
+// public interface IPrint
+// {
+//     void print();
+// }
+//
+// public interface IScan
+// {
+//     void scan();
+// }
+//
+// public class Scanner : IOn, IOff, IScan
+// {
+//     public void on()
+//     {
+//         Console.WriteLine("working");
+//     }
+//
+//     public void off()
+//     {
+//         Console.WriteLine("off");
+//     }
+//
+//     public void scan()
+//     {
+//         Console.WriteLine("scan");
+//     }
+// }
+//
+// public class Printer : IOn, IOff, IPrint
+// {
+//     public void on()
+//     {
+//         Console.WriteLine("working");
+//     }
+//     
+//     public void off()
+//     {
+//         Console.WriteLine("off");
+//     }
+//
+//     public void print()
+//     {
+//         Console.WriteLine("print");
+//     }
+//
+// }
+//
+// public class Copier:IOn,IOff,ICopy
+// {
+//     public void on()
+//     {
+//         Console.WriteLine("working");
+//     }
+//
+//     public void off()
+//     {
+//         Console.WriteLine("off");
+//     }
+//
+//     public void copy()
+//     {
+//         Console.WriteLine("copy");
+//     }
+// }
+// public class MultiDevice:IOn,IOff,ICopy,IScan,IPrint
+// {
+//     public void on()
+//     {
+//         Console.WriteLine("working");
+//     }
+//
+//     public void off()
+//     {
+//         Console.WriteLine("off");
+//     }
+//
+//     public void copy()
+//     {
+//         Console.WriteLine("copy");
+//     }
+//
+//     public void scan()
+//     {
+//         Console.WriteLine("scan");
+//     }
+//
+//     public void print()
+//     {
+//         Console.WriteLine("print");
+//     }
+// }
+    
+
+
+
+
---
+++ HW/HW_17_12_25/HW_17_12_25/Program.cs
@@ -0,0 +1,151 @@
+﻿namespace HW_17_12_25;
+
+// class Product
+// {
+//     private string name;
+//     private int price;
+//     private int quantity;
+//
+//     public int Price
+//     {
+//         get => price;
+//         set
+//         {
+//             if (value < 0)
+//                 throw new ArgumentException("Price cannot be negative");
+//             else
+//             {
+//                 price = value;
+//             }
+//         }
+//     }
+//
+//     public int Quantity
+//     {
+//         get => quantity;
+//         set
+//         {
+//             if (value < 0)
+//                 throw new ArgumentException("Quantity cannot be negative");
+//             else
+//             {
+//                 quantity = value;
+//             }
+//         }
+//     }
+//     public Product(string name, int price, int quantity)
+//         {
+//         this.name = name;
+//         this.price = price;
+//         this.quantity = quantity;
+//         }
+//
+//
+//     public void Input()
+//     {
+//         Console.Write("Please enter a name: ");
+//         this.name = Console.ReadLine();
+//         Console.Write("Please enter a price: ");
+//         this.price = int.Parse(Console.ReadLine());
+//         Console.Write("Please enter a quantity: ");
+//         this.quantity = int.Parse(Console.ReadLine());
+//
+//         
+//     }
+//
+//     public static Product operator +(Product p1, int value)
+//     {
+//
+//         return new Product(p1.name,p1.quantity+value,p1.price);
+//     }
+//
+//     public static Product operator -(Product p1, int value)
+//     {
+//         return new Product(p1.name, p1.quantity - value, p1.price);
+//     }
+//
+//     public static bool operator !=(Product p1, Product p2)
+//     {
+//         return !(p1 == p2);
+//     }
+//
+//     public static bool operator ==(Product p1, Product p2)
+//     {
+//         return p1.name == p2.name &&
+//                p1.price == p2.price &&
+//                p1.quantity == p2.quantity;
+//     }
+//
+//     public static bool operator <(Product p1, Product p2)
+//     {
+//         return p1.quantity < p2.quantity;
+//     }
+//
+//     public static bool operator >(Product p1, Product p2)
+//     {
+//         return p1.quantity > p2.quantity;
+//     }
+// }
+
+
+class TemperatureArray
+{
+    private int[] temperature = new int [7];
+
+    public int this[int index]
+    {
+        get { return temperature[index]; }
+        set { temperature[index] = value; }
+    }
+
+    public void Input()
+    {
+        int counter = 1;
+        
+        for (int i = 0; i < temperature.Length; i++)
+        {Console.WriteLine("day= : " + counter);
+            temperature[i] = int.Parse(Console.ReadLine());
+            
+            counter++;
+
+        }
+    }
+
+    public void Output()
+    {
+        int result1 = 0;
+        int result = 0;
+        for (int i = 0; i < temperature.Length; i++)
+        {
+            Console.Write(temperature[i] + " ");
+        }
+
+        for (int i = 0; i < temperature.Length; i++)
+        {
+             result += temperature[i]  ;
+        }
+
+        Console.WriteLine();
+        result1 = result / 7;
+        Console.WriteLine("average value= "+result1);
+    }
+}
+
+
+
+
+
+
+
+class Program
+
+
+{
+    static void Main(string[] args)
+    {
+        TemperatureArray temperatureArray = new TemperatureArray();
+        temperatureArray.Input();
+        temperatureArray.Output();
+        
+    }
+}
