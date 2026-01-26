b12bbf0acb8de5a08b571a066632eb1d143bdabf
hevingmey <grisenkov265@gmail.com>
2026-01-26 19:01:56 +0000

    add cw

---
+++ CW/Lesson_16/Lesson_16/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lesson_16;
+
+class Developer
+{
+    public string Name { get; set; }
+    public double Salary { get; set; }
+    public string Position { get; set; }
+    public Developer(string name, double salary, string position)
+    {
+        Name = name;
+        Salary = salary;
+        Position = position;
+    }
+    
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Developer d1 = new Developer("Alex", 1500, "midlle");
+        Developer d2 = new Developer("John", 1600, "midlle");
+        Developer d3 = new Developer("Jane", 2000, "senior");
+        Developer d4 = new Developer("Julia", 1900, "senior");
+        Developer d5 = new Developer("Reaigan", 1300, "junior");
+        List<Developer>people =new List<Developer>(){d1, d2, d3, d4, d5};
+            // групування обʼєктів по заданому параметру 
+        // var peopleGroup = from person in people
+        //     group person by person.Position;
+        // foreach (IGrouping<string, Developer> g in peopleGroup)
+        // {
+        //     Console.WriteLine(g.Key);
+        //     foreach (var w in g)
+        //         Console.WriteLine(w.Name);
+        // }
+
+        //Console.WriteLine('\n');
+            // групування для знаходження сумми зарплати для кожної групи 
+            
+        var people2= from person in people
+            group person by person.Position into g
+                select new { Name=  g.Key, Salary = g.Sum(s => s.Salary) };
+        foreach (var item in people2)
+            Console.WriteLine(item.Name+" "+item.Salary);
+        Console.WriteLine('\n');
+        
+    }
+}
---
+++ CW/Lesson_17/Lesson_17/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Lesson_17;
+
+
+class Unit
+{
+    private static int count = 0;
+  
+
+    public Guid ID { get; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public int Health { get; set; }
+
+   
+    private Unit(string name, int damage, int health)
+    {
+        ID = Guid.NewGuid();
+        Name = name;
+        Damage = damage;
+        Health = health;
+    }
+
+   
+    public static Unit Create(string name, int damage, int health)
+    {
+        if (count ==5)
+        {
+            Console.WriteLine("limits 5 units");
+            return null;
+        }
+
+        count++;
+        return new Unit(name, damage, health);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 8; i++)
+        {
+            Unit u = Unit.Create("Unit " + i, 10 * i, 100);
+
+            if (u != null)
+                Console.WriteLine($"created: {u.Name} | {u.ID}");
+        }
+    }
+}
