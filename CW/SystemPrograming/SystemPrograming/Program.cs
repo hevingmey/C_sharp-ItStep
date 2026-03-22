103b7dc5d828452b4f1ff29dbe4785ce5feac918
hevingmey <grisenkov265@gmail.com>
2026-03-22 18:15:09 +0000

    add new hw 3

---
+++ CW/SystemPrograming/SystemPrograming/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Threading;
+namespace SystemPrograming;
+
+internal class Program
+{
+    private static void PrintNormal()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("Normal");
+            Thread.Sleep(100);
+        }
+    }
+    private static void PrintLow()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("low");
+            Thread.Sleep(100);
+        }
+    }
+    private static void PrintHight()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("hight");
+            Thread.Sleep(100);
+        }
+    }
+    static void Main(string[] args)
+    {Thread threadLow = new Thread(PrintLow);
+        Thread threadNormal = new Thread(PrintNormal);
+              Thread threadHight = new Thread(PrintHight);
+
+        
+        
+        threadLow.Priority = ThreadPriority.Lowest;
+        threadNormal.Priority = ThreadPriority.Normal;
+
+        
+        threadHight.Priority = ThreadPriority.Highest;
+          
+        threadLow.Start();
+        threadNormal.Start();
+        threadHight.Start();
+        
+        threadLow.Join();
+        threadNormal.Join();
+        threadHight.Join();
+    }
+}
+
+
+    
+
+   
+
